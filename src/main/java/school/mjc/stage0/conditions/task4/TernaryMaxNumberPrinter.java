3593e6ac4060d88ad156deeffee22b351aa3440c
Masees85 <109299067+Masees85@users.noreply.github.com>
2022-08-09 14:35:21 -0400

    Update TernaryMaxNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -1,6 +1,17 @@
 package school.mjc.stage0.conditions.task4;
 
-public class TernaryMaxNumberPrinter {
-    public void printGreatest(int first, int second, int third) {
+public class TernaryMaxNumberPrinter 
+
+{
+    public void printGreatest(int n1, int n2, int n3) 
+    
+    {
+        max = (n1 > n2) ?
+          (n1 > n3 ? n1 : n3) :
+          (n2 > n3 ? n2 : n3);
+     
+    
+         System.out.println(max);
+        
     }
 }
